b448cc02338bc6e1201c6b929c94f83cd0048edf
Jonas Zeiger <jonas.zeiger@talpidae.net>
2018-11-18 14:30:17 +0100

    [RESTEASY-1142] Backport fix and tests to 3.6

--- testsuite/integration-tests/src/test/java/org/jboss/resteasy/test/exception/ClosedResponseHandlingTest.java
+++ testsuite/integration-tests/src/test/java/org/jboss/resteasy/test/exception/ClosedResponseHandlingTest.java
@@ -1,8 +1,6 @@
 package org.jboss.resteasy.test.exception;
 
 import java.lang.reflect.ReflectPermission;
-import java.util.HashMap;
-import java.util.Map;
 
 import javax.ws.rs.NotAcceptableException;
 import javax.ws.rs.NotSupportedException;
@@ -10,9 +8,7 @@ import javax.ws.rs.NotSupportedException;
 import org.jboss.arquillian.container.test.api.Deployment;
 import org.jboss.arquillian.container.test.api.RunAsClient;
 import org.jboss.arquillian.junit.Arquillian;
-import org.jboss.resteasy.client.jaxrs.internal.ResteasyClientBuilderImpl;
-import org.jboss.resteasy.plugins.server.servlet.ResteasyContextParameters;
-import org.jboss.resteasy.test.exception.resource.ClosedResponseHandlingEnableTracingRequestFilter;
+import org.jboss.resteasy.client.jaxrs.ResteasyClientBuilder;
 import org.jboss.resteasy.test.exception.resource.ClosedResponseHandlingPleaseMapExceptionMapper;
 import org.jboss.resteasy.test.exception.resource.ClosedResponseHandlingResource;
 import org.jboss.resteasy.utils.PermissionUtil;
@@ -26,7 +22,7 @@ import org.junit.runner.RunWith;
 /**
  * @tpSubChapter Resteasy-client
  * @tpChapter Integration tests
- * @tpSince RESTEasy 4.0.0.CR1
+ * @tpSince RESTEasy 3.6.3
  * @tpTestCaseDetails Regression test for RESTEASY-1142
  * @author <a href="ron.sigal@jboss.com">Ron Sigal</a>
  * @author <a href="jonas.zeiger@talpidae.net">Jonas Zeiger</a>
@@ -37,41 +33,36 @@ public class ClosedResponseHandlingTest {
 
    @Deployment
    public static Archive<?> deploy() {
-       WebArchive war = TestUtil.prepareArchive(ClosedResponseHandlingTest.class.getSimpleName());
-       war.addClass(ClosedResponseHandlingTest.class);
-       war.addPackage(ClosedResponseHandlingResource.class.getPackage());
-       war.addClass(PortProviderUtil.class);
-       war.addAsManifestResource(PermissionUtil.createPermissionsXmlAsset(
-             new ReflectPermission("suppressAccessChecks")
-       ), "permissions.xml");
+      WebArchive war = TestUtil.prepareArchive(ClosedResponseHandlingTest.class.getSimpleName());
+      war.addClass(ClosedResponseHandlingTest.class);
+      war.addPackage(ClosedResponseHandlingResource.class.getPackage());
+      war.addClass(PortProviderUtil.class);
+      war.addAsManifestResource(PermissionUtil.createPermissionsXmlAsset(
+           new ReflectPermission("suppressAccessChecks")
+      ), "permissions.xml");
 
-       Map<String, String> params = new HashMap<>();
-       params.put(ResteasyContextParameters.RESTEASY_TRACING_TYPE, ResteasyContextParameters.RESTEASY_TRACING_TYPE_ALL);
-       params.put(ResteasyContextParameters.RESTEASY_TRACING_THRESHOLD, ResteasyContextParameters.RESTEASY_TRACING_LEVEL_VERBOSE);
-
-       return TestUtil.finishContainerPrepare(war, params, ClosedResponseHandlingResource.class,
-             ClosedResponseHandlingPleaseMapExceptionMapper.class,
-             ClosedResponseHandlingEnableTracingRequestFilter.class);
+      return TestUtil.finishContainerPrepare(war, null, ClosedResponseHandlingResource.class,
+            ClosedResponseHandlingPleaseMapExceptionMapper.class);
     }
 
    /**
     * @tpTestDetails RESTEasy client errors that result in a closed Response are correctly handled.
     * @tpPassCrit A NotAcceptableException is returned
-    * @tpSince RESTEasy 4.0.0.CR1
+    * @tpSince RESTEasy 3.6.3
     */
    @Test(expected = NotAcceptableException.class)
    public void testNotAcceptable() {
-      new ResteasyClientBuilderImpl().build().target(generateURL("/testNotAcceptable")).request().get(String.class);
+      new ResteasyClientBuilder().build().target(generateURL("/testNotAcceptable")).request().get(String.class);
    }
 
    /**
     * @tpTestDetails Closed Response instances should be handled correctly with full tracing enabled.
     * @tpPassCrit A NotSupportedException is returned
-    * @tpSince RESTEasy 4.0.0.CR1
+    * @tpSince RESTEasy 3.6.3
     */
    @Test(expected = NotSupportedException.class)
    public void testNotSupportedTraced() {
-      new ResteasyClientBuilderImpl().build().target(generateURL("/testNotSupportedTraced")).request().get(String.class);
+      new ResteasyClientBuilder().build().target(generateURL("/testNotSupportedTraced")).request().get(String.class);
    }
 
    private static String generateURL(String path) {
